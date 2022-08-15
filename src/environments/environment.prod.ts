b3a75b42cee653546318c34ac0dd8f5a760f0041
Dawn Dixon <47092603+md56n@users.noreply.github.com>
2022-08-15 18:38:10 -0500

    remove firebase and build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: "AIzaSyBLuzCjv3L1kggsmv4uxuKepuLSVIxWTLw",
-    authDomain: "highland-cem.firebaseapp.com",
-    databaseURL: "https://highland-cem-default-rtdb.firebaseio.com",
-    projectId: "highland-cem",
-    storageBucket: "highland-cem.appspot.com",
-    messagingSenderId: "938767758932",
-    appId: "1:938767758932:web:f1d445a2c0bfd53a92dfc1",
-    measurementId: "G-9NSBG5LSLP"
-  },
   production: true
 };
